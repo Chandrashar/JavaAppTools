a459475385b663c57c957158e5c19d03ad023e8d
Chandrashar <chandrashar@gmail.com>
2018-07-02 23:52:27 +0530

    initial commit

---
+++ src/main/java/com/chan/app/Main.java
@@ -0,0 +1,13 @@
+package com.chan.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Chandra");
+
+        System.out.println(args[0] + " Chandra " + args[1]);
+
+
+    }
+}
---
+++ src/test/java/com/chan/app/MainTest.java
@@ -0,0 +1,22 @@
+package com.chan.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void mainTest() {
+
+        assertEquals("Chandra", "Chandra");
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
